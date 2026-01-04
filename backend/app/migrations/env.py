0ce6e8f974010af6ac9fe36d390c66bf68d72e11
sujal gawas <sujalgawas18@gmail.com>
2026-01-05 04:30:43 +0530

    small fixes

--- backend/app/migrations/env.py
+++ backend/app/migrations/env.py
@@ -4,9 +4,9 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-from app.models import users
-from app.models import test
-from app.db.base import Base
+from models import users
+from models import test
+from db.base import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
