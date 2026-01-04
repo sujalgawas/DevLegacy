67cd202ddb3c76a61cf35d011baa8c9f1ae2a042
sujal gawas <sujalgawas18@gmail.com>
2026-01-05 02:28:50 +0530

    postgresql added

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,14 +2,39 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from firebase_admin import credentials,initialize_app,auth
 import json
+from sqlalchemy import create_engine,Column,String,Integer
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 path = "../serviceAccountKey.json"
 
+engine = create_engine("postgresql://postgres:1234@localhost:5432/dev")
+Session = sessionmaker(engine)
+session = Session()
+
+Base = declarative_base()
+#================ database models start ================#
+"""
+CREATE TABLE users(
+	uid VARCHAR(100) PRIMARY KEY,
+	Email VARCHAR(50),
+	UserName VARCHAR(50)
+);
+"""
+
+class User(Base):
+    __tablename__ = 'users'
+    uid = Column(String, primary_key=True)
+    email = Column(String)
+    username = Column(String)
+
+#================ database models end ================#
 cred = credentials.Certificate(path)
 initialize_app(cred)
 
 app = FastAPI()
 
+#================ FastAPI BaseModel Start ================#
 class Login(BaseModel):
     token : str
     
@@ -17,7 +42,8 @@ class SignUp(BaseModel):
     UserName : str
     Email : str
     Password: str
-    
+#================ FastAPI BaseModel End ================#
+
 #helper function
 def verify_token(token):
     decoded_token = auth.verify_id_token(token)
@@ -39,10 +65,13 @@ def login(login:Login):
 def signup(signup:SignUp):
     user = auth.create_user(email= signup.Email,password=signup.Password,
                             display_name = signup.UserName)
+    current_uid = user.uid
     
-    #postgres SQL verfication + session activation
+    user = User(uid = current_uid,email = signup.Email, username = signup.UserName)
+    session.add(user)
+    session.commit()
     
-    if user.uid():
+    if user.uid:
         return {"message":"user created"},200
     else:
         return {"message":"Error creating user"},401
