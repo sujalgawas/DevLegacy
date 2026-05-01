874c3866e0bf028aa486002b330ceebb50767911
sujalgawas <sujalgawas18@gmail.com>
2026-05-02 00:49:14 +0530

    fix migration and docker-compose for db

--- backend/app/crud/User.py
+++ backend/app/crud/User.py
@@ -12,7 +12,10 @@ from app.models.document_stat import document_stats
 from app.models.code import Code
 from app.models.code_quality import Code_quality
 
-engine = create_engine("postgresql://postgres:1234@localhost:5432/dev")
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:1234@host.docker.internal:5432/dev")
+engine = create_engine(DATABASE_URL)
 
 def get_session():
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
