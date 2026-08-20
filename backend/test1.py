559813c4cbceaa9bb6d822eaf3ab03cd9fb20fff
sujal gawas <sujalgawas18@gmail.com>
2026-08-20 08:22:55 +0530

    removing manual .py test files

--- backend/test1.py
+++
@@ -1,4 +0,0 @@
-from app.services.codequality import code_quality
-
-text = {"project1":["sdfjkdsfkjsdjfskfjsdfjdk"],"project2":["sdfjkdsfkjsdjfskfjsdfjdk"],"project3":["sdfjkdsfkjsdjfskfjsdfjdk"],}
-print(code_quality(text))
